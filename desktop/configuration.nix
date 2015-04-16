c615e799d04ce4a61278601ec34ddb120d4318d9
David Ackerman <david.w.ackerman@gmail.com>
2015-04-16 07:13:41 -0700

    Created desktop config

---
+++ desktop/configuration.nix
@@ -0,0 +1,85 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+  
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sdb";
+
+  networking.hostName = "immutable-oranges"; # Define your hostname.
+  networking.hostId = "1a9f99d1";
+
+  nixpkgs.config = {
+    allowUnfree = true;
+    
+    chromium = {
+      enablePepperFlash = true;
+      enablePepperPDF = true;
+    };
+  };
+  
+  time.timeZone = "US/Pacific";
+
+  environment.systemPackages = with pkgs; [
+    python27Full
+    git
+    dmenu2
+    terminator
+    chromium
+    wget
+    vim
+    emacs
+    emacs24Packages.dash
+    emacs24Packages.haskellMode
+    haskellPackages.xmonad
+    haskellPackages.xmobar
+  ];
+
+  services.openssh.enable = true;
+
+  services.printing.enable = true;
+
+  hardware.opengl.driSupport32Bit = true;
+
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    videoDrivers = [ "nvidia" ];
+
+    xrandrHeads = [ "DVI-I-1" "DVI-D-0" ];
+
+    desktopManager = {
+      default = "none";
+      xterm.enable = false;
+    };
+
+    displayManager = {
+      lightdm = {
+        enable = true;
+      };
+    };
+
+    windowManager = {
+      xmonad = {
+       enable = true;
+       enableContribAndExtras = true;
+       extraPackages = self: [ self.xmonadContrib ];
+      };
+      default = "xmonad";
+    };
+  };
+
+  users.extraUsers.david = {
+    extraGroups = [ "wheel" ];
+    isNormalUser = true;
+    uid = 1000;
+  };
+}
