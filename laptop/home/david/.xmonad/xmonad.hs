d637b52e5a539d69465712461cdc5115bf18b3e3
David Ackerman <david.w.ackerman@gmail.com>
2022-01-15 08:25:21 -0800

    Add startup programs for xmonad

--- laptop/home/david/.xmonad/xmonad.hs
+++ laptop/home/david/.xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
+import Data.Monoid (mconcat, (<>))
 
 import SharedConfig
 
@@ -13,9 +14,16 @@ main = do
 
   let baseConfig = sharedConfig xmproc
 
+  let startupPrograms =
+        [ "signal-desktop"
+        , "emacs"
+        , "google-chrome-stable"
+        ]
+
   xmonad $ (
     baseConfig { workspaces = ["1:code", "2:term", "3:web", "4", "5", "6:entertainment", "7:music", "8", "9"]
                , manageHook = manageHook baseConfig
+               , startupHook = mconcat (spawn <$> startupPrograms) <> startupHook baseConfig
                , handleEventHook = moveSignalToCurrentWindowHook
                , modMask = myModMask
                } `additionalKeys` (sharedKeyMap myModMask)
