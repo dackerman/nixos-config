4abfd96b640d1d9e2795fd8cd538710189f13ae8
David Ackerman <david.w.ackerman@gmail.com>
2022-09-08 11:20:13 -0700

    Added flutter

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -78,6 +78,7 @@
     android-studio
     leiningen
     clj-kondo                 # clojure linting support in editor
+    flutter                   # android app development with dart
 
     # Applications
     signal-desktop            # chat application
