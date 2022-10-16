44528c3f9aae64f426ef97b1feb7eb665ef83d94
David Ackerman <david.w.ackerman@gmail.com>
2022-10-16 08:21:45 -0700

    fix typo

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -186,20 +186,12 @@
   users.users.david = {
     isNormalUser = true;
     description = "David Ackerman";
-    extraGroups = [ "networkmanager" wheel" "audio" ];
+    extraGroups = [ "networkmanager" "wheel" "audio" ];
     # openssh.authorizedKeys.keyFiles = [
     #   "/home/david/.ssh/id_rsa.pub"
     #   "/home/david/.ssh/laptop_rsa.pub"
     # ];
   };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
 
   system.autoUpgrade.enable = true;
 }
